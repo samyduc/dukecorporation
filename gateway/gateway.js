b32623ecbc46be23a9f2a45dd9ced82b9a4e2f09
samy duc <samy.duc@gmail.com>
2013-05-08 14:59:54 +0200

    Node js module : rabbitjs & socket.io

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -1,8 +1,16 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(80, '0.0.0.0');
-console.log('Server running at http://127.0.0.1:1337/');
+// require
+var socketio = require('socket.io');
+var rabbitmq = require('rabbit.js');
+
+// init
+var rabbitcontext = rabbitmq.createContext('amqp://localhost');
+var io = socketio.listen(80);
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
+});
 
 

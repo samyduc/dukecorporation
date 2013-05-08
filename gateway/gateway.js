01da228f7fbb5caaac45907da87725222e423e19
samy duc <samy.duc@gmail.com>
2013-05-08 15:39:50 +0200

    Basic structure for node js -> need to be tested

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -7,9 +7,29 @@ var rabbitcontext = rabbitmq.createContext('amqp://localhost');
 var io = socketio.listen(80);
 
 io.sockets.on('connection', function (socket) {
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function (data) {
-		console.log(data);
+
+	var pub = context.socket('PUB');
+	var sub = context.socket('SUB');
+
+	pub.setEncoding('utf8');
+	sub.setEncoding('utf8');
+
+	pub.connect("pub:".concat(socket.id))
+	sub.connect("sub:".concat(socket.id))
+
+	connection.on('message', function(msg) {
+    	pub.write(msg);
+	});
+
+	sub.on('data', function(msg) {
+		connection.send(msg);
+	});
+
+	connection.on('disconnect', function() {
+		sub.write(JSON.stringify({'event': 'disconnect'}))
+
+		pub.destroy();
+		sub.destroy();
 	});
 });
 

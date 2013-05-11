4ce5691b580e63f4965b169cffac55a3963e5c13
Marine Duc <malie.bywater@gmail.com>
2013-05-11 18:01:39 +0200

    only vote if you"r in the room

--- client/js/systems/RandomDeathRoom.js
+++ client/js/systems/RandomDeathRoom.js
@@ -6,45 +6,49 @@ RandomDeathRoomSystem = pc.systems.EntitySystem.extend('RandomDeathRoomSystem',
     {},
     {
 
-        init:function ()
-        {
+        init: function () {
             this._super([ 'RandomDeathRoom' ]);
         },
 
-        process:function (entity)
-        {
+        process: function (entity) {
             var randomDeathComponent = entity.getComponent('randomdeathroom');
             var basicRoomComponent = entity.getComponent('basicroom');
+
+            var currentPlayer = entity.layer.scene.player.getComponent('player');
+
+
             basicRoomComponent.updateSurvivorsList();
-            var random = Math.floor(Math.random()*100);
+            var random = Math.floor(Math.random() * 100);
             var tempPlayerList = [];
-                for (var i=0,len=basicRoomComponent.players.length; i<len; i++)
-                {
-                  var isSurvivor = false;
-                  for(var j=0; j < basicRoomComponent.survivors.length; j++ ){
-                      if( basicRoomComponent.players[i] == basicRoomComponent.survivors[j]){
-                             isSurvivor = true;
-                             break;
-                      }
-                  }
-                  if(!isSurvivor){
-                      if(random <= randomDeathComponent.killRate){
-                        this.sendVoteDead(basicRoomComponent.players[i].username, basicRoomComponent);
-                      }else{
-                          tempPlayerList[tempPlayerList.length] = basicRoomComponent.players[i];
-                          basicRoomComponent.survivors[basicRoomComponent.survivors.length] = basicRoomComponent.players[i];
-                      }
-                  }else{
-                      tempPlayerList[tempPlayerList.length] = basicRoomComponent.players[i];
-                  }
+            for (var i = 0, len = basicRoomComponent.players.length; i < len; i++) {
+                var isSurvivor = false;
+                for (var j = 0; j < basicRoomComponent.survivors.length; j++) {
+                    if (basicRoomComponent.players[i] == basicRoomComponent.survivors[j]) {
+                        isSurvivor = true;
+                        break;
+                    }
                 }
-                basicRoomComponent.players = tempPlayerList;
+                if (!isSurvivor) {
+                    if (random <= randomDeathComponent.killRate) {
+                        //vote only if you are in the room
+                        if (currentPlayer.roomId == basicRoomComponent.id) {
+                            this.sendVoteDead(basicRoomComponent.players[i].username, basicRoomComponent);
+                        }
+                    } else {
+                        tempPlayerList[tempPlayerList.length] = basicRoomComponent.players[i];
+                        basicRoomComponent.survivors[basicRoomComponent.survivors.length] = basicRoomComponent.players[i];
+                    }
+                } else {
+                    tempPlayerList[tempPlayerList.length] = basicRoomComponent.players[i];
+                }
+            }
+            basicRoomComponent.players = tempPlayerList;
         },
 
         sendVoteDead: function (playerName, room_component) {
             var socket = pc.device.game.socket;
-            var message =  { event: 'vote_dead', room: room_component.id, username: playerName};
-            socket.emit('message', message );
+            var message = { event: 'vote_dead', room: room_component.id, username: playerName};
+            socket.emit('message', message);
             console.log("send message:");
             console.log(message);
         }
